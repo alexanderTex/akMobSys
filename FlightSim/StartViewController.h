025a2664c03a779c0dda9d34c703df712c2d08d6
s0549906 <s0549906@htw-berlin.de>
2017-01-17 15:45:01 +0100

    added startviewcontroller ( cocou class -> identifier panel assignment ; toDO viewcontroller + tabel

---
+++ FlightSim/StartViewController.h
@@ -0,0 +1,13 @@
+//
+//  StartViewController.h
+//  FlightSim
+//
+//  Created by  on 17/01/17.
+//  Copyright © 2017 a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartViewController : UIViewController
+
+@end
